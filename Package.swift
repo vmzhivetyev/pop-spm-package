46dea906c195df9e7090c9de65cb710458fd5178
Viacheslav Zhivetyev <slavyan6363@gmail.com>
2021-01-28 18:59:23 +0300

    added platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "pop",
+    platforms: [ .iOS(.v11) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,6 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "pop", path: "pop.xcframework")
+        .binaryTarget(name: "pop", path: "pop.xcframework"),
+        .testTarget(name: "POPPackageTests")
     ]
 )
--- Tests/POPPackageTests/POPPackageTests.swift
+++ Tests/POPPackageTests/POPPackageTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import POPPackage
+@testable import pop
 
 final class POPPackageTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(POPPackage().text, "Hello, World!")
+        POPAnimation()
     }
 
     static var allTests = [
